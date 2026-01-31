6aa04b38ea905c7fa7cdfe7f0b56abe0307a545d
mirna-salem <ms.mirnasalem@gmail.com>
2026-01-30 20:17:36 -0600

    Use Link from react-router

--- src/Pages/OrdersPage.jsx
+++ src/Pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from "../components/Header";
+import { Link } from 'react-router';
 import "./OrdersPage.css";
 
 export function OrdersPage() {
@@ -56,11 +57,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
 
               <div className="product-image-container">
@@ -85,11 +86,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -134,11 +135,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
