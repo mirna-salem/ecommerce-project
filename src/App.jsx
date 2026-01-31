c454cff5921b9a7a1a54aa2a6c661a1cb8567bb6
mirna-salem <ms.mirnasalem@gmail.com>
2026-01-30 20:53:03 -0600

    Create page not found

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { HomePage } from './Pages/HomePage';
 import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { OrdersPage } from './Pages/OrdersPage';
 import { TrackingPage } from './Pages/TrackingPage';
+import { NotFoundPage } from './Pages/NotFoundPage';
+
 import './App.css'
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Route path="/checkout" element={<CheckoutPage />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
     
   );
---
+++ src/Pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header.jsx";
+import "./NotFoundPage.css";
+
+export function NotFoundPage() {
+  return (
+    <div className="not-found">
+      <Header />
+      <h1>404</h1>
+      <p>OH NO! Page not found!!!</p>
+    </div>
+  );
+}
